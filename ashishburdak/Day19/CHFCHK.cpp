8485e20bc5eb2b9954a9622ed9477579dc4c3f90
ashishburdak <45838960+ashishburdak@users.noreply.github.com>
2020-01-20 00:49:49 +0530

    create CHFCHK.cpp

---
+++ ashishburdak/Day19/CHFCHK.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define mod 1000000007
+#define ll long long
+#define ld long double
+#define pb push_back
+#define ff first
+#define ss second
+#define all(x) x.begin(), x.end()
+#define mp map<long, long>
+#define um unordered_map<long, long>
+#define pll pair<long, long>
+#define st set<long> 
+#define us unordered_set<long>
+#define vt vector<long>
+#define vp vector<pll>
+#define fl(i, x, y) for(long i=x;i<y;++i)
+#define flr(i, x, y) for(long i=x;i>=y;--i)
+
+int main() {
+	// your code goes here
+	int t,n;
+	cin>>t;
+	while(t--)
+	{
+	    cin>>n;
+	    long a[n];
+	    fl(i, 0, n)
+	    cin>>a[i];
+	    sort(a,a+n);
+	    cout<<a[0]<<endl;
+	}
+	return 0;
+}
