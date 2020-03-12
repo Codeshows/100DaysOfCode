b36459b3a8341debab8f50344c2a51b86e901b1b
vedant3620 <49477120+vedant3620@users.noreply.github.com>
2020-03-12 12:36:49 +0530

    Day 42, 43 and 44 (#331)
    * Day 42
    
    * Day 43
    
    * Day 44

---
+++ vedant3620/day42/1312a.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+int main(){
+	ll t;
+	cin>>t;
+	while(t--){
+		ll n,m;
+		cin>>n>>m;
+		if(n%m==0){
+			cout<<"YES"<<endl;
+		}
+		else{
+			cout<<"NO"<<endl;
+		}
+	}
+}
