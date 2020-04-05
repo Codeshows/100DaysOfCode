a64ea4676364849e186c4fc2738e6919d3d5c63b
MAYANK CHHIPA <40556224+mgroovyank@users.noreply.github.com>
2020-04-05 10:58:49 +0530

    Create Tushar's Birthday Party(Recursive).cpp

---
+++ mgroovyank/Day 96/Tushar's Birthday Party(Recursive).cpp
@@ -0,0 +1,34 @@
+//
+// Time Complexity: O(Friends * Dishes * Capacity)
+
+int dp[1001][1001];
+int knapSack(const vector<int> &A, const vector<int> &B, const vector<int> &C, int i, int cap){
+    if (cap == 0){
+        return 0;
+    }
+    if (i >= B.size() || cap<0){
+        return INT_MAX;
+    }
+    if(dp[i][cap] != -1){
+        return dp[i][cap];
+    }
+    if(cap >= B[i]){
+        int temp = knapSack(A, B, C, i, cap - B[i]);// here it seems that i is not out of
+        //range but, it can be when cap - B[i]< 0
+        if(temp == INT_MAX){
+            return dp[i][cap] = knapSack(A, B, C, i+1, cap);
+        }
+        return dp[i][cap] = min(C[i] + temp, knapSack(A, B, C, i+1, cap));
+    }
+    return dp[i][cap] = knapSack(A, B, C, i+1, cap);
+}
+
+int Solution::solve(const vector<int> &A, const vector<int> &B, const vector<int> &C) {
+    int n = B.size();
+    memset(dp, -1, sizeof(dp));
+    int ans = 0;
+    for (int i =0; i < A.size(); i++) {
+        ans += knapSack(A, B, C, 0, A[i]);
+    }
+    return ans;
+}
